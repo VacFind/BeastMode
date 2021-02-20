1acb40ec98c396d3b9739a64e8e8a9117d34ba0e
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-19 23:21:36 -0800

    update all.sh

--- all.sh
+++ all.sh
@@ -1,3 +1,8 @@
-while IFS= read -r line; do
- 	whois "$line" >> whois.txt
-done < all.txt
+#!/bin/bash
+
+# US postal abbreveations
+for x in {al,ak,as,az,ar,ca,co,ct,de,dc,fm,fl,ga,gu,hi,id,il,in,ia,ks,ky,la,me,mh,md,ma,mi,mn,ms,mo,mt,ne,nv,nh,nj,nm,ny,nc,nd,mp,oh,ok,or,pw,pa,pr,ri,sc,sd,tn,tx,ut,vt,vi,va,wa,wv,wi,wy}
+do
+	echo "vaccinate$x.com" 
+done
+
