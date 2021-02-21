539e175839f0a8d5458773b09c724bfa583a073e
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-20 19:49:27 -0800

    add TLD generation to the list

--- all.sh
+++ all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# US postal abbreveations
-for x in {al,ak,as,az,ar,ca,co,ct,de,dc,fm,fl,ga,gu,hi,id,il,in,ia,ks,ky,la,me,mh,md,ma,mi,mn,ms,mo,mt,ne,nv,nh,nj,nm,ny,nc,nd,mp,oh,ok,or,pw,pa,pr,ri,sc,sd,tn,tx,ut,vt,vi,va,wa,wv,wi,wy}
+for tld in {com,net,org}
 do
-	echo "vaccinate$x.com" 
+	# US postal abbreveations
+	for x in {al,ak,az,ar,ca,co,ct,de,fl,ga,hi,id,il,in,ia,ks,ky,la,me,md,ma,mi,mn,ms,mo,mt,ne,nv,nh,nj,nm,ny,nc,nd,oh,ok,or,pa,ri,sc,sd,tn,tx,ut,vt,va,wa,wv,wi,wy,dc,as,gu,mp,pr,vi}
+	do
+		# main="${x}vaccinate"
+		echo "${x}vaccinate.$tld" 
+	done
 done
 
