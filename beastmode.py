37be7fe5cb7182605349c299726f183007fe2472
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-21 17:33:50 -0800

    convert to python

--- beastmode.py
+++ beastmode.py
@@ -1,11 +1,9 @@
 
-for tld in {com,net,org}
-do
-	# US postal abbreveations
-	for x in {al,ak,az,ar,ca,co,ct,de,fl,ga,hi,id,il,in,ia,ks,ky,la,me,md,ma,mi,mn,ms,mo,mt,ne,nv,nh,nj,nm,ny,nc,nd,oh,ok,or,pa,ri,sc,sd,tn,tx,ut,vt,va,wa,wv,wi,wy,dc,as,gu,mp,pr,vi}
-	do
-		# main="${x}vaccinate"
-		echo "${x}vaccinate.$tld" 
-	done
-done
+state_postal_codes = ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "dc", "as", "gu", "mp", "pr", "vi"]
+
+tlds = ["com", "net", "org"]
+
+for tld in tlds:
+	for x in state_postal_codes:
+		print( x + "vaccinate" + "." + tld )
 
