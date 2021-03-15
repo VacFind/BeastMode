09b136fe89cafce9e3d7e6cacdb0bf99af63207d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-14 20:44:10 -0700

    use new pattern-based generator for getting a list of domains

--- beastmode.py
+++ beastmode.py
@@ -1,9 +1,3 @@
+from patterns import generate_names
 
-state_postal_codes = ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "dc", "as", "gu", "mp", "pr", "vi"]
-
-tlds = ["com", "net", "org"]
-
-for tld in tlds:
-	for x in state_postal_codes:
-		print( x + "vaccinate" + "." + tld )
-
+print(generate_names("covid{state_name}.{tld}"))
