3b4478ec549c1d8bff97c4be60cb4cc00216cd2a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-14 20:40:24 -0700

    add a module for parsing patterns into a list of domain names

---
+++ patterns.py
@@ -0,0 +1,40 @@
+import itertools
+
+patterns = {
+	"state_code": ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "dc", "as", "gu", "mp", "pr", "vi"],
+	"state_name": ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming", "district of columbia", "american samoa", "guam", "northern mariana islands", "puerto rico", "virgin islands"],
+	"tld": ["com", "net", "org"]
+}
+
+def get_tokens(pattern_string):
+	# split on the opening curlybrace to mark the start of the token
+	token_locations = pattern_string.split("{")
+	del token_locations[0]
+	tokens = []
+	for tokenchunk in token_locations:
+		# split on the closing curlybrace to separate the end of the token from the rest (which we dont care about for the purposes of this function)
+		token = tokenchunk.split("}")[0]
+		tokens.append(token.strip())
+	return tokens
+
+def get_iterations(tokens):
+	values = []
+	for token in tokens:
+		if patterns[token] is not None:
+			values.append(patterns[token])
+		else:
+			values.append([])
+	return list(itertools.product(*values))
+
+
+def generate_names(pattern_string):
+	tokens = get_tokens(pattern_string)
+	iterations = get_iterations(tokens)
+	names = []
+
+	for iteration in iterations:
+		substitutions = dict([(tokens[i], iteration[i]) for i in range(0,len(tokens))])
+		name = pattern_string.format(**substitutions)
+		names.append(name)
+	
+	return names
