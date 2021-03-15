351feead78db56e1043f292b62abf69b39fd8475
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-15 09:05:01 -0700

    use new db function to add new names to the db

--- beastmode.py
+++ beastmode.py
@@ -1,3 +1,7 @@
 from patterns import generate_names
+from database import add_from_name_list
 
-print(generate_names("covid{state_name}.{tld}"))
+names = generate_names("coronavirus{state_name}.{tld}")
+
+add_from_name_list(names)
+
