dc7bb31cf12c30ee34643c014c63da12139a8db5
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-16 12:25:34 -0700

    refactor database file into a class

--- database.py
+++ database.py
@@ -4,22 +4,23 @@ from sqlalchemy.orm import sessionmaker
 from models import Base, Domain
 import logging
 
-logger = logging.getLogger(__name__)
+class BeastModeDB:
+	def __init__(self, connection_string='sqlite:///domains.db'):
+		self.logger = logging.getLogger(__name__)
+		self.session = self.new_session(connection_string=connection_string)
 
-def new_session(connection_string='sqlite:///domains.db'):
-	engine = create_engine(connection_string, echo=False)
-	Base.metadata.create_all(engine)
-	Session = sessionmaker(bind=engine)
-	return Session()
+	def new_session(self, connection_string='sqlite:///domains.db'):
+		engine = create_engine(connection_string, echo=False)
+		Base.metadata.create_all(engine)
+		Session = sessionmaker(bind=engine)
+		return Session()
 
-
-
-def add_domains(domains, dryrun=False):
-	try:
-		if not dryrun:
-			session.add_all(domains)
-			session.commit()
-	except SQLAlchemyError as e:
-		session.rollback()
-		logger.error("An error occurred while inserting into database")
-		logger.error(e)
+	def add_domains(self, domains, dryrun=False):
+		try:
+			if not dryrun:
+				self.session.add_all(domains)
+				self.session.commit()
+		except SQLAlchemyError as e:
+			session.rollback()
+			self.logger.error("An error occurred while inserting into database")
+			self.logger.error(e)
