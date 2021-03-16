7e7b9f0ef344b5600c46e35384c9aad69de20b54
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-16 12:23:00 -0700

    add better logging to whois.py

--- whois.py
+++ whois.py
@@ -1,5 +1,8 @@
 import requests
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 def get_whois_for_domain(domain, dryrun=False):
 	body = "query=" + domain.domainname
@@ -8,7 +11,7 @@ def get_whois_for_domain(domain, dryrun=False):
 		"Referer": "https://www.bulkseotools.com/bulk-whois-lookup.php"
 	}
 	if dryrun:
-		print("would fetch whois for " + domain.domainname)
+		logger.info("would fetch whois for " + domain.domainname)
 		return
 
 	r = requests.post('https://www.bulkseotools.com/json/bulk-whois-lookup.php', data=body, headers=headers)
@@ -16,9 +19,8 @@ def get_whois_for_domain(domain, dryrun=False):
 	if r.status_code == 200:
 		return r.json()
 	else:
-		print("fetching whois for " + domain.domainname + " failed with code " + r.status_code)
-		print("response received")
-		print(vars(r))
+		logger.error("fetching whois for " + domain.domainname + " failed with code " + r.status_code)
+		logger.debug("response: " + vars(r))
 		return
 
 def get_whois_for_domain_list(domains, dryrun=False):
