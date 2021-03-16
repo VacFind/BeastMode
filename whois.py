30e21c15a73aca6ed322abb2f63fc1dbbac48994
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-16 10:45:34 -0700

    add dryrun parameters to whois functions too

--- whois.py
+++ whois.py
@@ -1,12 +1,16 @@
 import requests
 import time
 
-def get_whois_for_domain(domain):
+def get_whois_for_domain(domain, dryrun=False):
 	body = "query=" + domain.domainname
 	headers= {
 		"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
 		"Referer": "https://www.bulkseotools.com/bulk-whois-lookup.php"
 	}
+	if dryrun:
+		print("would fetch whois for " + domain.domainname)
+		return
+
 	r = requests.post('https://www.bulkseotools.com/json/bulk-whois-lookup.php', data=body, headers=headers)
 
 	if r.status_code == 200:
@@ -17,9 +21,9 @@ def get_whois_for_domain(domain):
 		print(vars(r))
 		return
 
-def get_whois_for_domain_list(domains):
+def get_whois_for_domain_list(domains, dryrun=False):
 	for domain in domains:
-		whois = get_whois_for_domain(domain)
+		whois = get_whois_for_domain(domain, dryrun=dryrun)
 		if whois:
 			domain.set_whois_data(whois)
 		
