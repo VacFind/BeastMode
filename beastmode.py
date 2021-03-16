78a777eda1af50de6d64372876ff6e601891aa2c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-16 08:59:28 -0700

    add dryrun option

--- beastmode.py
+++ beastmode.py
@@ -1,19 +1,31 @@
 from patterns import generate_domains
 from database import add_domains
 
-def generate_from_pattern_file(filename):
+import argparse
+
+parser = argparse.ArgumentParser(description='generate and check a list of domains from patterns')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--patternfile', type=str)
+group.add_argument('--pattern', "-p", type=str)
+parser.add_argument("--dryrun", "-n", action="store_true")
+
+
+def generate_from_pattern_file(filename, dryrun=False):
 	with open(filename, "r") as patterns_file:
 		for pattern in patterns_file.readlines():
-			process_pattern(pattern)
+			process_pattern(pattern, dryrun=dryrun)
 
-def process_pattern(pattern):
+def process_pattern(pattern, dryrun=False):
 	pattern = pattern.strip()
 	print("processing pattern: " + pattern)
 	domains = generate_domains(pattern)
-	try:
-		add_domains(names)
-	except exc.SQLAlchemyError as e:
-		print("An error occurred. skipping this pattern")
-		print(e)
+	if not dryrun:
+		try:
+			add_domains(names)
+		except exc.SQLAlchemyError as e:
+			print("An error occurred. skipping this pattern")
+			print(e)
+	else:
+		print("Would process " + str(len(domains)) + " domains")
 
 
