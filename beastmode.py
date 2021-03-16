d357c1716c1fad4d2ce861202d5c059e34bfc89c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-03-16 08:42:42 -0700

    split processing a single pattern into a new function

--- beastmode.py
+++ beastmode.py
@@ -4,13 +4,16 @@ from database import add_domains
 def generate_from_pattern_file(filename):
 	with open(filename, "r") as patterns_file:
 		for pattern in patterns_file.readlines():
-			pattern = pattern.strip()
-			print("processing pattern: " + pattern)
-			domains = generate_domains(pattern)
-			try:
-				add_domains(names)
-			except exc.SQLAlchemyError as e:
-				print("An error occurred. skipping this pattern")
-				print(e)
+			process_pattern(pattern)
+
+def process_pattern(pattern):
+	pattern = pattern.strip()
+	print("processing pattern: " + pattern)
+	domains = generate_domains(pattern)
+	try:
+		add_domains(names)
+	except exc.SQLAlchemyError as e:
+		print("An error occurred. skipping this pattern")
+		print(e)
 
 
